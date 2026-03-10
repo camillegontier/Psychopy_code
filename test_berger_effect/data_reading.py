a82e3d1f7ec7f576a955ce7a04e425916aadb874
Camille Gontier <cam.gontier@gmail.com>
2026-03-10 11:21:30 +0100

    Correct stream name

--- test_berger_effect/data_reading.py
+++ test_berger_effect/data_reading.py
@@ -23,7 +23,7 @@ streams, header = pyxdf.load_xdf(dir_path)
 plt.figure()
 
 for stream in streams:
-    if stream["info"]["name"][0] == 'EEG':
+    if stream["info"]["name"][0] == 'psychopy_markers':
         for t in stream["time_stamps"]:
             plt.axvline(x=t,linestyle = '--')
     elif stream["info"]["name"][0] == 'obci_eeg1':
